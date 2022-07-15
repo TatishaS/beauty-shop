4a7ccaeadaf3dd926bf6375570200a95320dba15
TatishaS <snow_white_girl@bk.ru>
2022-07-15 16:38:07 +0300

    More optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import Loadable from 'react-loadable';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
 
-//import products from './products.json';
+const Cart = Loadable({
+  loader: () => import(/* webpackChunkName: "Cart"*/ './pages/Cart'),
+  loading: () => <div>Подождите немного, корзина загружается...</div>,
+});
 
 function App() {
   return (
@@ -15,7 +18,16 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/cart"
+            element={
+              <Suspense
+                fallback={<div>Подождите немного, идет загрузка...</div>}
+              >
+                <Cart />
+              </Suspense>
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
